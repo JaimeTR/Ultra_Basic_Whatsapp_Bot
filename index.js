764e09bb41967852dab77fd71d5c4fa164e63898
Jaime Tarazona <jaime130798@gmail.com>
2021-09-10 09:57:54 -0500

    10-09

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const fs = require('fs');
-const { Client } = require('whatsapp-web.js');
-const schedule = require('node-schedule');
+import { existsSync, writeFile } from 'fs';
+import { Client } from 'whatsapp-web.js';
+import { scheduleJob } from 'node-schedule';
 
 const recipient = process.env.RECIPIENT;
 const scheduleTime = process.env.TIME;
@@ -9,25 +9,25 @@ let isReady = false;
 
 const SESSION_FILE_PATH = './session.json';
 let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
+if (existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 
 
 /* Add headless: true as a param if running from a VPS or Cloud Server */
-const client = new Client({ puppeteer: {  }, session: sessionCfg });
+const client = new Client({ puppeteer: {}, session: sessionCfg });
 
 client.initialize();
 
 client.on('qr', (qr) => {
-    /* Encode QR to something useful, could be printed to the terminal using QRCode-Terminal or Emailing it to you */ 
+    /* Encode QR to something useful, could be printed to the terminal using QRCode-Terminal or Emailing it to you */
     console.log('QR RECEIVED', qr);
 });
 
 client.on('authenticated', (session) => {
     console.log('AUTHENTICATED', session);
-    sessionCfg=session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+    sessionCfg = session;
+    writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
         if (err) {
             console.error(err);
         }
@@ -40,9 +40,7 @@ client.on('auth_failure', msg => {
 });
 
 // TODO: Add more messages
-const goodDays = [
-    'Buenos días, que tengas un día bonito:) ❤️',
-];
+const goodDays = ['Buenos días, que tengas un día bonito: ❤️', ];
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
@@ -51,7 +49,7 @@ function getRandomArbitrary(min, max) {
 client.on('ready', () => {
     console.log('READY, Waiting for Scheduler');
     isReady = true;
-    
+
 });
 
 /* Cron-like library, use Cron, I was lazy and I did not wanted to add a Cron Job so I had to use this library
@@ -64,20 +62,19 @@ client.on('ready', () => {
                                 │ │ │ │ │
                                 │ │ │ │ │                    */
 //var j = schedule.scheduleJob('* * * * *', () => {
-var j = schedule.scheduleJob(scheduleTime, () => {
-    
-    if(!isReady){
+var j = scheduleJob(scheduleTime, () => {
+
+    if (!isReady) {
         console.error("User not ready");
         process.exit(1);
     }
 
     let y = Math.round(getRandomArbitrary(0, goodDays.length));
-    client.sendMessage(`${recipient}@c.us`,goodDay[y]).then((response)=>{
-        if(response.id.fromMe){
+    client.sendMessage(`${recipient}@c.us`, goodDay[y]).then((response) => {
+        if (response.id.fromMe) {
             console.log("done");
             //process.exit(1)
         }
     });
 
 });
-
